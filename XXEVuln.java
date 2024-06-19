81aff7a3057003e5bc1381ee9a4cf9b0c624f581
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2024-06-19 17:35:49 -0400

    Create XXEVuln.java

---
+++ XXEVuln.java
@@ -0,0 +1,43 @@
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+/** Holds various XXE vulns for different APIs. */
+public class XXEVuln {
+
+    public String docToString(final Document poDocument) throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource domSrc = new DOMSource(poDocument);
+        StringWriter sw = new StringWriter();
+        StreamResult result = new StreamResult(sw);
+        transformer.transform(domSrc, result);
+        return sw.toString();
+    }
+
+    public void saxTransformer() throws ParserConfigurationException, SAXException, IOException {
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        spf.setValidating(true);
+
+        SAXParser saxParser = spf.newSAXParser();
+        XMLReader xmlReader = saxParser.getXMLReader();
+        xmlReader.parse("file.xml");
+    }
+
+    public Document withDom(String xml) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        return db.parse(new InputSource(new StringReader(xml)));
+    }
+}
